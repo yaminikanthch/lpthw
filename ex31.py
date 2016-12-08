06f182f45a2e52f60953029db82ef97acd852523
yaminikanthch <yaminikanthch@gmail.com>
2016-12-08 22:41:24 +0530

    new execercise on maths

---
+++ ex31.py
@@ -0,0 +1,16 @@
+# demo on the variable excercise.
+print "i will now demo the chickens"
+
+print "hens", 25 + 30 /6
+print "roosters", 100 -25 *3 %4
+
+print " Now i will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+
+print " Is it true that 3 + 2 < 5 - 7 ? "
+
+print 3 + 2 < 5 - 7
+
+print " what is 3 + 2 ?", 3 + 2
+
---
+++ ex4.py
@@ -0,0 +1,14 @@
+# demo on some maths
+
+print "This is an example on maths"
+
+# Lets do some addition
+print "Add", 3 + 2
+print "Sub", 100 - 50
+print "Mul", 4 * 3
+print "Div", 4 / 2
+print "Which is greater? 5 > -2 ", 5 > -2
+print "Is it greater or equal? 8 >= 2", 8 >= 2
+print "Which is lesser? 4 < 2", 4 < 2
+
+
