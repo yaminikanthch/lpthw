e7365d5e0ef41b9ee0ec1517267446b082e20e47
yaminikanthch <yaminikanthch@gmail.com>
2016-12-12 23:46:18 +0530

    New staring example with formatter

---
+++ ex8.py
@@ -0,0 +1,7 @@
+formatter = "%r %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one","Two","Three","Four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+
