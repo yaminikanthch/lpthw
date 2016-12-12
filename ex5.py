c8a2da2536f92307daccd88d835a177180839c01
yaminikanthch <yaminikanthch@gmail.com>
2016-12-12 15:17:03 +0530

    new example add for variable and printing

---
+++ ex5.py
@@ -0,0 +1,10 @@
+# Example to demo variable and printing
+my_name = "Yaminikanth"
+my_height =5.8 # Feet and inches
+my_age = 34
+my_hair = "black" #this is not a joke 
+
+print " Lets talk about yor name %s" % my_name
+print " My height is %d" % my_height
+print " My age is %d"  % my_age
+print " My Height %d, does not match with my age %d " % (my_height,my_age)
