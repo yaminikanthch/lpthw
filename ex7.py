f5640251b02bf919500944dfb5b81ef4be562fff
yaminikanthch <yaminikanthch@gmail.com>
2016-12-12 22:40:34 +0530

    New excecise on sting print

---
+++ ex7.py
@@ -0,0 +1,17 @@
+# This excercise we will learn about more print options
+
+my_name = "Yaminikanth"
+my_age = 34 # this is a lie
+
+print "My Name is %s , age is %d " % (my_name,my_age)
+print "This is new one for print %s" % "Sita"
+print "." * 10
+print "%" * 10
+end1 = "YA"
+end2 = "MI"
+end3 = "NI"
+end4 = "KA"
+end5 = "NTH"
+print end1 + end2 + end3,
+print end4 + end5
+
