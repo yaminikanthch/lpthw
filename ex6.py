a9b5b9961f31a1ef0abccfa5406cf3153644fa17
yaminikanthch <yaminikanthch@gmail.com>
2016-12-12 15:53:30 +0530

    new example on string and text

---
+++ ex6.py
@@ -0,0 +1,11 @@
+my_age = 34 
+my_name = "Yaminikanth"
+
+print " Hello %s my age is %d" % (my_name,my_age)
+
+print " There are %d types of people" % 10
+
+my_bool = False
+my_test = " This is just test of boolean %r"
+
+print my_test % my_bool
