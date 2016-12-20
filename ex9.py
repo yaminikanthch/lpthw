92c952ad983af1e5f8c2fcc27a9451b25d7d5d41
yaminikanthch <yaminikanthch@gmail.com>
2016-12-20 13:05:25 +0530

    new example with new lines

--- ex9.py
+++ ex9.py
@@ -6,4 +6,9 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug\nSept\nOct\nNov\nDec"
 print "here are the days : ",days
 print "here are the months : ", months
 
+print """
+There is some thing new with three double quotes.
+Try with some other.
+Hope this help.
+"""
 
