74528c3a6103a52ed394a8142528903fe09e56e3
yaminikanthch <yaminikanthch@gmail.com>
2016-12-20 13:07:02 +0530

    new example with new lines

--- ex9.py
+++ ex9.py
@@ -1,7 +1,7 @@
 #Demo on some more prints.
 
 days = "Mon Tue Wed Thur Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug\nSept\nOct\nNov\nDec"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug\nSept\nOct\nNov\nDec"
 
 print "here are the days : ",days
 print "here are the months : ", months
