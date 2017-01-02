cd128ec67e50725f903acb1d09e4622fdb0ee84b
yaminikanthch <yaminikanthch@gmail.com>
2017-01-02 17:12:26 +0530

    raw input example added

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print "What is your age? "
+age = raw_input()
+print "What is your name? "
+name = raw_input()
+print "What is your sex? "
+sex = raw_input()
+
+print "My name  is %r,%r, and i am %r years old" % (name,sex,age)
