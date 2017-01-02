002e3f539a14121792d518168d0df7f112baa608
yaminikanthch <yaminikanthch@gmail.com>
2017-01-02 17:01:55 +0530

    new execercise on split and list added

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tab1 = "\tIam tab1 in."
+tab2  = "Iam split\non a line."
+tab3 = "I'm \\ a \\ cat."
+
+list_all = """
+I will do a list:
+\t* list1
+\t* list2
+\t* list3\n\t* list4
+"""
+
+print tab1
+print tab2
+print tab3
+print list_all
