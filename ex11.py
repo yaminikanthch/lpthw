19ba21ba8beac00c3c6c292bc6363b4cf0230964
yaminikanthch <yaminikanthch@gmail.com>
2017-01-02 17:35:03 +0530

    new example on prompt input added

--- ex11.py
+++ ex11.py
@@ -4,5 +4,8 @@ print "What is your name? "
 name = raw_input()
 print "What is your sex? "
 sex = raw_input()
+print "My phone number is"
+phone = int(raw_input())
 
+my_age = raw_input("How old are you ?")
 print "My name  is %r,%r, and i am %r years old" % (name,sex,age)
---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = raw_input("hello what is your age?")
+name = raw_input("What is your name?")
+height = raw_input("my height is")
+
+print "My name is %r, my age is %r and height is %r" % (name,age,height)
